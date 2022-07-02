7ce55ff445f2680079c88d1a31f59f9fbaf59983
rackdon <r.martinez@ride-on.es>
2022-07-02 09:20:18 +0200

    Format code

--- src/app.ts
+++ src/app.ts
@@ -22,14 +22,16 @@ const server = http.createServer(app)
 sentryConfig.init(app)
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  )
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
   } else {
     next()
-
   }
 })
 app.use(morgan('common'))
